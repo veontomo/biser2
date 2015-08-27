dda7bc56fab84c8228f74019171a1ef5a5c12002
Mario Rossi <veontomo@gmail.com>
2015-08-27 08:32:24 +0200

    feat: add a listview to the layout for similar beads

--- app/src/main/java/com/veontomo/biser2/api/BeadAdapter.java
+++ app/src/main/java/com/veontomo/biser2/api/BeadAdapter.java
@@ -4,24 +4,47 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.List;
+
 /**
  * Adapter to display information about beads
  *
  */
 public class BeadAdapter extends BaseAdapter {
+    /**
+     * Array of bead color codes
+     */
+    private List<String> items;
+
+    /**
+     * Number of different layouts
+     */
+    private final short NUM_OF_TYPES = 2;
+
+    /**
+     * Identifier of the first layout
+     */
+    private final short TYPE_PRESENT = 0;
+
+    /**
+     * Identifier of the second layout
+     */
+    private final short TYPE_ABSENT = 1;
+
+
     @Override
     public int getCount() {
-        return 0;
+        return this.items.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return this.items.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
